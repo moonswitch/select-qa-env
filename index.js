8c40389d68d0b5e07faafbc059348bc40d9e9176
Jeff French <jeff.french@moonswitch.com>
2022-01-05 14:01:43 -0600

    First cut of action code

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const Firestore = require('@google-cloud/firestore');
+const github = require('@actions/github');
 
+const db = new Firestore();
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const table_name = core.getInput('table');
+    const table = db.collection(table_name);
+    const number = github.context.issue.number;
+    const pr = `pr-${number}`
+    let env;
+    let url;
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // First check if there is already an environment assigned to this PR.
+    const current_envs = await table.where('pr', '==', pr).where('in_use', '==', true).limit(1).get();
 
-    core.setOutput('time', new Date().toTimeString());
+    if (!current_envs.empty) {
+      env = current_envs[0];
+    } else {
+      // No environment currently assigned. Check for available environments.
+      const envs = await table.where('in_use', '==', false).limit(1).get()
+
+      // No environments available. Bail out.
+      if (envs.empty) {
+        throw new Error('No QA environments available.');
+      }
+  
+      env = envs[0];
+      // Mark the environment as in_use and set the PR for reference.
+      env.update({in_use: true, pr})
+    }
+
+    url = env.data().url; 
+    core.setOutput('url', url);
   } catch (error) {
     core.setFailed(error.message);
   }
